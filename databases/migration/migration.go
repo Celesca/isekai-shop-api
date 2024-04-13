ab7da4388a248d7b7cafe3026af01ead443d602d
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-14 01:38:59 +0700

    🔐 add: migration

--- databases/migration/migration.go
+++ databases/migration/migration.go
@@ -1,3 +1,17 @@
 // Run the schema migration for the database
 // Run through function main
 package main
+
+import (
+	"fmt"
+
+	"github.com/Celesca/isekai-shop-api/config"
+	"github.com/Celesca/isekai-shop-api/databases"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+
+	fmt.Println(db.ConnectionGetting())
+}
