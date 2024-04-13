04535ab55cc949183b5891d89aae311bc44c03f7
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-13 19:00:32 +0700

    map: config.yaml and config.go

--- config/config.go
+++ config/config.go
@@ -1,3 +1,54 @@
 package config
 
+import "time"
+
 // สิ่งที่เปลี่ยนแปลงได้ตลอดเวลา deploy ขึ้น cloud
+
+type (
+	Config struct {
+		Server   *Server   `mapstructure:"server" validate:"required"`
+		OAuth2   *OAuth2   `mapstructure:"oauth2" validate:"required"`
+		State    *State    `mapstructure:"state" validate:"required"`
+		Database *Database `mapstructure:"database" validate:"required"`
+	}
+
+	Server struct {
+		Port         int           `mapstructure:"port" validate:"required"`
+		AllowOrigins []string      `mapstructure:"allowOrigins" validate:"required"`
+		TimeOut      time.Duration `mapstructure:"timeout" validate:"required"`
+		BodyLimit    string        `mapstructure:"bodyLimit" validate:"required"`
+	}
+
+	OAuth2 struct {
+		PlayerRedirectUrl string   `mapstructure:"playerRedirectUrl" validate:"required"`
+		AdminRedirectUrl  string   `mapstructure:"adminRedirectUrl" validate:"required"`
+		ClientID          string   `mapstructure:"clientId" validate:"required"`
+		ClientSecret      string   `mapstructure:"clientSecret" validate:"required"`
+		Endpoints         endpoint `mapstructure:"endpoints" validate:"required"`
+		Scopes            []string `mapstructure:"scopes" validate:"required"`
+		UserInfoUrl       string   `mapstructure:"userInfoUrl" validate:"required"`
+		RevokeUrl         string   `mapstructure:"revokeUrl" validate:"required"`
+	}
+
+	endpoint struct {
+		AuthUrl       string `mapstructure:"authUrl" validate:"required"`
+		TokenUrl      string `mapstructure:"tokenUrl" validate:"required"`
+		DeviceAuthUrl string `mapstructure:"deviceAuthUrl" validate:"required"`
+	}
+
+	State struct {
+		Secret    string        `mapstructure:"secret" validate:"required"`
+		ExpiresAt time.Duration `mapstructure:"expiresAt" validate:"required"`
+		Issuer    string        `mapstructure:"issuer" validate:"required"`
+	}
+
+	Database struct {
+		Host     string `mapstructure:"host" validate:"required"`
+		Port     int    `mapstructure:"port" validate:"required"`
+		User     string `mapstructure:"user" validate:"required"`
+		Password string `mapstructure:"password" validate:"required"`
+		DBName   string `mapstructure:"dbname" validate:"required"`
+		SSLMode  string `mapstructure:"sslmode" validate:"required"`
+		Schema   string `mapstructure:"schema" validate:"required"`
+	}
+)
