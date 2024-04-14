3f95efcf1a4bca17f990bec333a654be618bb0e6
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-14 23:30:12 +0700

    🎉  add: air

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/Celesca/isekai-shop-api/config"
+	"github.com/Celesca/isekai-shop-api/databases"
+	"github.com/Celesca/isekai-shop-api/server"
+)
+
 func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
 
 }
