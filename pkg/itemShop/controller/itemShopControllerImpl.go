b47352d0b79dc4d55406f49a05f5513e1e8daf57
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-26 22:44:21 +0700

    add: itemShopRouter

--- pkg/itemShop/controller/itemShopControllerImpl.go
+++ pkg/itemShop/controller/itemShopControllerImpl.go
@@ -11,7 +11,7 @@ type itemShopControllerImpl struct {
 	itemShopService _itemShopService.ItemShopService
 }
 
-func NewItemShopController(itemShopService _itemShopService.ItemShopService) ItemShopController {
+func NewItemShopControllerImpl(itemShopService _itemShopService.ItemShopService) ItemShopController {
 	return &itemShopControllerImpl{itemShopService}
 }
 
