d4609fe026e0ee256353b3fc42e6079873799b8e
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-26 22:55:23 +0700

    fix: custom error json

--- pkg/itemShop/controller/itemShopControllerImpl.go
+++ pkg/itemShop/controller/itemShopControllerImpl.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"net/http"
 
+	"github.com/Celesca/isekai-shop-api/pkg/custom"
+
 	_itemShopService "github.com/Celesca/isekai-shop-api/pkg/itemShop/service"
 	"github.com/labstack/echo/v4"
 )
@@ -18,7 +20,7 @@ func NewItemShopControllerImpl(itemShopService _itemShopService.ItemShopService)
 func (c *itemShopControllerImpl) Listing(pctx echo.Context) error {
 	itemModelList, err := c.itemShopService.Listing()
 	if err != nil {
-		return pctx.String(http.StatusInternalServerError, err.Error())
+		return custom.Error(pctx, http.StatusInternalServerError, err.Error())
 	}
 
 	return pctx.JSON(http.StatusOK, itemModelList)
