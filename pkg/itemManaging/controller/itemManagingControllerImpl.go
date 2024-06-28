cb325fdb7a77e42be165c192485bb0540ed02c6c
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-28 23:39:37 +0700

    init: itemManaging structure

--- pkg/itemManaging/controller/itemManagingControllerImpl.go
+++ pkg/itemManaging/controller/itemManagingControllerImpl.go
@@ -1,5 +1,15 @@
 package controller
 
-func NewItemManagingController() ItemManagingController {
-	return &itemManagingControllerImpl{}
+import (
+	_itemManagingService "github.com/Celesca/isekai-shop-api/pkg/itemManaging/service"
+)
+
+type itemManagingControllerImpl struct {
+	itemManagingService _itemManagingService.ItemManagingService
+}
+
+func NewItemManagingControllerImpl(
+	itemManagingService _itemManagingService.ItemManagingService,
+) ItemManagingController {
+	return &itemManagingControllerImpl{itemManagingService}
 }
