45e8c77cd59e959bbb9da27a2f482c3bcaab6302
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-10 18:07:37 +0700

    add: service and controller itemManaging

--- pkg/itemManaging/controller/itemManagingControllerImpl.go
+++ pkg/itemManaging/controller/itemManagingControllerImpl.go
@@ -1,7 +1,12 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/Celesca/isekai-shop-api/pkg/custom"
+	_itemManagingModel "github.com/Celesca/isekai-shop-api/pkg/itemManaging/model"
 	_itemManagingService "github.com/Celesca/isekai-shop-api/pkg/itemManaging/service"
+	"github.com/labstack/echo/v4"
 )
 
 type itemManagingControllerImpl struct {
@@ -12,4 +17,21 @@ func NewItemManagingControllerImpl(
 	itemManagingService _itemManagingService.ItemManagingService,
 ) ItemManagingController {
 	return &itemManagingControllerImpl{itemManagingService}
+}
+
+func (c *itemManagingControllerImpl) Creating(pctx echo.Context) error {
+	itemCreatingReq := new(_itemManagingModel.ItemCreatingReq)
+
+	customEchoRequest := custom.NewCustomEchoRequest(pctx)
+
+	if err := customEchoRequest.Bind(itemCreatingReq); err != nil {
+		return custom.Error(pctx, http.StatusBadRequest, err.Error())
+	}
+
+	item, err := c.itemManagingService.Creating(itemCreatingReq)
+	if err != nil {
+		return custom.Error(pctx, http.StatusInternalServerError, err.Error())
+	}
+
+	return pctx.JSON(http.StatusCreated, item)
 }
--- pkg/itemManaging/service/itemManagingServiceImpl.go
+++ pkg/itemManaging/service/itemManagingServiceImpl.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"github.com/Celesca/isekai-shop-api/entities"
+	_itemManagingModel "github.com/Celesca/isekai-shop-api/pkg/itemManaging/model"
 	_itemManagingRepository "github.com/Celesca/isekai-shop-api/pkg/itemManaging/repository"
+	_itemShopModel "github.com/Celesca/isekai-shop-api/pkg/itemShop/model"
 )
 
 type itemManagingServiceImpl struct {
@@ -12,4 +15,21 @@ func NewItemManagingServiceImpl(
 	itemManagingRepository _itemManagingRepository.ItemManagingRepository,
 ) ItemManagingService {
 	return &itemManagingServiceImpl{itemManagingRepository}
+}
+
+func (s *itemManagingServiceImpl) Creating(itemCreatingReq *_itemManagingModel.ItemCreatingReq) (*_itemShopModel.Item, error) {
+	// Entity to send to repository
+	itemEntity := &entities.Item{
+		Name:        itemCreatingReq.Name,
+		Description: itemCreatingReq.Description,
+		Picture:     itemCreatingReq.Picture,
+		Price:       itemCreatingReq.Price,
+	}
+
+	itemEntityResult, err := s.itemManagingRepository.Creating(itemEntity)
+	if err != nil {
+		return nil, err
+	}
+
+	return itemEntityResult.ToItemModel(), nil
 }
