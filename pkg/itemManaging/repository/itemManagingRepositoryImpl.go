1852ba76575feca32b391a1162fc4afbd9c81d59
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-10 17:58:55 +0700

    add: itemManagingRepositoryImpl

--- pkg/itemManaging/repository/itemManagingRepositoryImpl.go
+++ pkg/itemManaging/repository/itemManagingRepositoryImpl.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"github.com/Celesca/isekai-shop-api/entities"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+
+	_itemManagingException "github.com/Celesca/isekai-shop-api/pkg/itemManaging/exception"
 )
 
 type itemManagingRepositoryImpl struct {
@@ -15,4 +18,17 @@ func NewItemManagingRepositoryImpl(db *gorm.DB, logger echo.Logger) *itemManagin
 		db:     db,
 		logger: logger,
 	}
+}
+
+func (r *itemManagingRepositoryImpl) Creating(itemEntity *entities.Item) (*entities.Item, error) {
+
+	// allocate new item to return
+	item := new(entities.Item)
+
+	if err := r.db.Create(itemEntity).Error; err != nil {
+		r.logger.Errorf("Creating item failed: %v", err)
+		return nil, &_itemManagingException.ItemCreating{}
+	}
+
+	return item, nil
 }
